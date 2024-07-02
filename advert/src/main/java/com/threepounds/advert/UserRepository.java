4fefd0315a2704f0efffc1bdd2f22e0273cbd52d
bkantarRDC <bkantar@idbs.com>
2024-07-02 09:22:08 +0300

    db connection and user entity added

---
+++ advert/src/main/java/com/threepounds/advert/UserRepository.java
@@ -0,0 +1,11 @@
+package com.threepounds.advert;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
---
+++ advert/src/main/java/com/threepounds/advert/UserService.java
@@ -0,0 +1,24 @@
+package com.threepounds.advert;
+
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+
+  public User save(User user){
+    return userRepository.save(user);
+  }
+
+  public List<User> list(){
+    return userRepository.findAll();
+  }
+
+
+}
