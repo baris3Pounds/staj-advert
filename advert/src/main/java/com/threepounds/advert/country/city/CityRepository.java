9646c52a05b825b49851105910e396ff4c055561
mehmetakifsalman <ahmetakifsalman@gmail.com>
2024-07-21 21:55:55 +0300

    All pages updated

--- advert/src/main/java/com/threepounds/advert/country/city/CityRepository.java
+++ advert/src/main/java/com/threepounds/advert/country/city/CityRepository.java
@@ -1,12 +1,13 @@
 package com.threepounds.advert.country.city;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface CityRepository extends JpaRepository<City, UUID> {
+public interface CityRepository extends PagingAndSortingRepository<City, UUID> {
     List<City> findAll();
 
     Optional<City> findById(UUID id);
