f53d129c57d09a8d30b8370a5677f171b03f9a0a
mehmetakifsalman <ahmetakifsalman@gmail.com>
2024-07-24 20:20:53 +0300

    Locatin api added

---
+++ advert/src/main/java/com/threepounds/advert/RestTemplateTrain/RestTemplateService.java
@@ -0,0 +1,18 @@
+package com.threepounds.advert.RestTemplateTrain;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RestTemplateService {
+
+    private final RestTemplate restTemplate;
+
+    public Location getLocation(String ip){
+       return restTemplate.getForObject("http://ip-api.com/json/" + ip , Location.class);
+    }
+
+    public RestTemplateService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+}
