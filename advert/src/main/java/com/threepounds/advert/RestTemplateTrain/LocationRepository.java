3d8b679e8d2ca2811184d645ef4e29bbc8def0e1
mehmetakifsalman <ahmetakifsalman@gmail.com>
2024-07-29 17:28:50 +0300

    Location entity added

---
+++ advert/src/main/java/com/threepounds/advert/RestTemplateTrain/LocationRepository.java
@@ -0,0 +1,8 @@
+package com.threepounds.advert.RestTemplateTrain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LocationRepository extends JpaRepository<Location , UUID> {
+}
