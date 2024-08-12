edcb4aba843cd249050a14b541ceaab8189e5068
bkantarRDC <bkantar@idbs.com>
2024-08-12 09:38:00 +0300

    postgres

--- advert/src/main/java/com/threepounds/advert/rolePermisionUser/utils/mapper/UserMapper.java
+++ advert/src/main/java/com/threepounds/advert/rolePermisionUser/utils/mapper/UserMapper.java
@@ -5,10 +5,15 @@ import com.threepounds.advert.rolePermisionUser.resource.UserResource;
 import org.mapstruct.Mapper;
 
 import java.util.List;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+     @Mapping(target = "roles" , ignore = true)
      User userDtoToEntity(UserDto signupDto);
+
+     @Mapping(target = "roles" , ignore = true)
      User userDtoToUser(UserDto userDto);
      UserResource userToUserResource(User user);
      UserResource findByName(String name);
--- advert/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO ads(id,title, description, price, active)
-VALUES ('98fd92fe-dc20-4050-abf3-7815dad68218','ad-1', 'a remarkable ad', 12.43, true),
- ('98fd92fe-dc20-4050-abf3-7815dad68217','ad-2', 'standart ad', 122.10, true);
