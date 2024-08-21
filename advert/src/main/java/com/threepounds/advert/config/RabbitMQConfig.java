77ff7226340aee320e2b1769b5c9bf5551fc0371
bkantarRDC <bkantar@idbs.com>
2024-08-21 13:09:02 +0300

    rabbit template message converter updated

--- advert/src/main/java/com/threepounds/advert/config/RabbitMQConfig.java
+++ advert/src/main/java/com/threepounds/advert/config/RabbitMQConfig.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -53,6 +54,11 @@ public class RabbitMQConfig {
   @Bean
   public Binding bindingAdvertDeleted(@Qualifier("queueAdvertDeleted") Queue queue, DirectExchange exchange) {
     return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+  }
+
+  @Bean
+  public Jackson2JsonMessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
   }
 
 
